de168fa76e24c67119ac87850bfe998de3b4af68
SchneiderSix <104461916+SchneiderSix@users.noreply.github.com>
2022-12-20 01:35:47 -0300

    Improving script, querySelector

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,21 +1,16 @@
 #!/usr/bin/node
-const checkbox = document.getElementsByClassName('ckbx');
-const mydiv = document.getElementsByClassName('amenities.h4');
-const myarr = [];
-const mydict = {};
+window.onload = function() {
+  const qs = document.querySelector('div.amenities h4');
+  //qs.innerHTML = "<b>foo</b>";
+  const checkbox = document.getElementsByClassName('ckbx');
+  const mydict = {};
 
-checkbox.addEventListener('change', (event) => {
-  if (event.currentTarget.checked) {
-    mydict[checkbox.dataset.id] = checkbox.dataset.name;
-    for (const i of mydict) {
-      if (i === 0) {
-        myarr.push(mydict[i]);
-      } else {
-        myarr.push(', ' + mydict[i]);
-      }
-    };
-  } else {
-    delete mydict[checkbox.dataset.id];
-  }
-});
-return mydiv.innerHTML = myarr;
+  checkbox.addEventListener('change', (event) => {
+    if (event.currentTarget.checked) {
+      mydict[checkbox.dataset.id] = checkbox.dataset.name;
+    } else {
+      delete mydict[checkbox.dataset.id];
+    }
+  });
+  qs.innerHTML = mydict;
+};
