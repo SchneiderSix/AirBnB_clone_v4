ff65771b94cbb51075d66c5d0b23541cdb8ecbd3
SchneiderSix <https://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@github.com>
2022-12-22 16:40:15 +0000

    Update red circle

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,49 +1,50 @@
 #!/usr/bin/node
-window.onload = function() {
-        const qs = document.querySelector('div.amenities h4');
-        const bx = document.getElementsByTagName("input");
-        const le = bx.length;
-        const arr = [];
-      
-        for (let i = 0; i < le; i++) {
-          let checkbox = document.getElementsByTagName("input")[i];
-          checkbox.addEventListener('change', (event) => {
-            if (event.currentTarget.checked) {
-              if (String(qs.innerHTML) === '&nbsp;') {
-                arr.push(checkbox.dataset.name);
-                qs.innerHTML = arr.join(', ');
-              } else {
-                arr.push(checkbox.dataset.name);
-                qs.innerHTML = arr.join(', ');
-              };
-            } else if (event.currentTarget.checked === false) {
-              const idx = arr.indexOf(checkbox.dataset.name);
-              arr.splice(idx, 1);
-              if (arr.length === 0) {
-                qs.innerHTML = '&nbsp;';
-              } else {
-                qs.innerHTML = arr.join(', ');
-              };
-            };
-          });
-        };
+const qs = document.querySelector("div.amenities h4");
+const bx = document.getElementsByTagName("input");
+const le = bx.length;
+const arr = [];
 
-        async function makeRequest() {
-          try {
-            const response = await fetch('http://0.0.0.0:5001/api/v1/status/');
-      
-            const circle = document.querySelector('#api_status');
-            if (response.status === 'OK') {
-              circle.classList.add('available');
-            } else {
-              circle.classList.remove('available');
-            };
+for (let i = 0; i < le; i++) {
+  let checkbox = document.getElementsByTagName("input")[i];
+  checkbox.addEventListener("change", (event) => {
+    if (event.currentTarget.checked) {
+      if (String(qs.innerHTML) === "&nbsp;") {
+        arr.push(checkbox.dataset.name);
+        qs.innerHTML = arr.join(", ");
+      } else {
+        arr.push(checkbox.dataset.name);
+        qs.innerHTML = arr.join(", ");
+      }
+    } else if (event.currentTarget.checked === false) {
+      const idx = arr.indexOf(checkbox.dataset.name);
+      arr.splice(idx, 1);
+      if (arr.length === 0) {
+        qs.innerHTML = "&nbsp;";
+      } else {
+        qs.innerHTML = arr.join(", ");
+      }
+    }
+  });
+}
 
-          } catch (err) {
-            console.log(err);
-          }
-        }
-  
-        makeRequest();
-};
-      
+async function makeRequest() {
+  try {
+    const response = await fetch("http://localhost:5001/api/v1/status/");
+
+    const data = await response.json();
+
+    const circle = document.getElementById("api_status");
+
+    console.log(data);
+
+    if (data.status === "OK") {
+      circle.classList.add("available");
+    } else {
+      circle.classList.remove("available");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+makeRequest();
